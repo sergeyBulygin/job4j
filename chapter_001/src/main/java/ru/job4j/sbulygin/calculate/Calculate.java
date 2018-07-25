30246a16df4abc9ee560540a7e7949fc6f224448
sergeyBulygin <sergteletrade@gmail.com>
2018-07-25 13:00:04 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/sbulygin/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/sbulygin/calculate/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j.sbulygin.calculate;
 
+/**
+ * Colculate class.
+ * @author sbulygin.
+ * @since 25.07.2018.
+ * @version 1.0.
+ */
 public class Calculate {
 
+    /**
+     * Main.
+     * @param args do not use.
+     */
     public static void main(String[] args) {
       System.out.println("Hello world");
     }
---
+++ chapter_001/src/main/java/ru/job4j/sbulygin/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for calculate task.
+*
+* @param arg - arg
+* @author sbulygin
+* @since 25.07.2018
+* @version 1.0
+*/
+package ru.job4j.sbulygin.calculate;
