9698806fadeb1dd99431ce97aec510b165d46c8d
sergeyBulygin <sergteletrade@gmail.com>
2018-10-02 10:17:33 +0300

    Additional test task.

--- chapter_005/src/main/java/ru/job4j/sbulygin/check/Verification.java
+++ chapter_005/src/main/java/ru/job4j/sbulygin/check/Verification.java
@@ -1,5 +1,8 @@
 package ru.job4j.sbulygin.check;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Verification class.
  * @author sbulygin.
@@ -9,5 +12,32 @@ package ru.job4j.sbulygin.check;
 public class Verification {
 
 
+    public boolean checkWords(String wordOne, String wordTwo) {
+
+        if (wordOne == null || wordTwo == null || wordOne.length() != wordTwo.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < wordOne.length(); i++) {
+
+            char charWordOne = wordOne.charAt(i);
+            int  counterCharsWordOne = map.containsKey(charWordOne) ? map.get(charWordOne) : 0;
+            map.put(charWordOne, ++counterCharsWordOne);
+
+            char charWordTwo = wordTwo.charAt(i);
+            int counterCharsWordTwo = map.containsKey(charWordTwo) ? map.get(charWordTwo) : 0;
+            map.put(charWordTwo, --counterCharsWordTwo);
+
+        }
+
+        for (int countMap : map.values()) {
+            if (countMap != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
--- chapter_005/src/test/java/ru/job4j/sbulygin/check/VerificationTest.java
+++ chapter_005/src/test/java/ru/job4j/sbulygin/check/VerificationTest.java
@@ -1,5 +1,11 @@
 package ru.job4j.sbulygin.check;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * VerificationTest class.
  * @author sbulygin.
@@ -8,6 +14,76 @@ package ru.job4j.sbulygin.check;
  */
 public class VerificationTest {
 
+    private Verification test;
 
+    @Before
+    public void prepareForTest() {
+
+        test = new Verification();
+
+    }
+
+    @Test
+    public void whenTwoSameWordsThenResultTrue() {
+
+        boolean expected = test.checkWords("papa", "papa");
+
+        assertThat(true, is(expected));
+
+    }
+
+    @Test
+    public void whenTwoDifferentWordsThenResultFalse() {
+
+        boolean expected = test.checkWords("papa", "mama");
+
+        assertThat(false, is(expected));
+
+    }
+
+    @Test
+    public void whenTwoAnagramWordsThenResultTrue() {
+
+        boolean expected = test.checkWords("papa", "apap");
+
+        assertThat(true, is(expected));
+
+    }
+
+    @Test
+    public void whenTwoLongWordsThenResultTrue() {
+
+        boolean expected = test.checkWords("papaaapapap", "apappaapapa");
+
+        assertThat(true, is(expected));
+
+    }
+
+    @Test
+    public void whenTwoLongWordsHaveOtherSymbolOneThenResultFalse() {
+
+        boolean expected = test.checkWords("papaaapapap", "apapoaapapa");
+
+        assertThat(false, is(expected));
+
+    }
+
+    @Test
+    public void whenTwoDifferentLengthWordsThenResultFalse() {
+
+        boolean expected = test.checkWords("papaaapapap", "apappaa");
+
+        assertThat(false, is(expected));
+
+    }
+
+    @Test
+    public void whenTwoOtherSymbolWordsThenResultTrue() {
+
+        boolean expected = test.checkWords("&&**%$#", "*%*&#&$");
+
+        assertThat(true, is(expected));
+
+    }
 
 }
