9e0e2cda43b6eba5cfa345880073c73b8bd0b8cb
sergeyBulygin <sergteletrade@gmail.com>
2018-10-13 12:24:09 +0300

    Refactoring test task #2.

--- chapter_005/src/main/java/ru/job4j/sbulygin/check/Verification.java
+++ chapter_005/src/main/java/ru/job4j/sbulygin/check/Verification.java
@@ -10,34 +10,30 @@ import java.util.Map;
  * @version 1.0.
  */
 public class Verification {
-
     public boolean checkWords(String wordOne, String wordTwo) {
-
         if (wordOne == null || wordTwo == null || wordOne.length() != wordTwo.length()) {
             return false;
         }
-
         Map<Character, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < wordOne.length(); i++) {
-
-            char charWordOne = wordOne.charAt(i);
-            int  counterCharsWordOne = map.containsKey(charWordOne) ? map.get(charWordOne) : 0;
-            map.put(charWordOne, ++counterCharsWordOne);
-
-            char charWordTwo = wordTwo.charAt(i);
-            int counterCharsWordTwo = map.containsKey(charWordTwo) ? map.get(charWordTwo) : 0;
-            map.put(charWordTwo, --counterCharsWordTwo);
-
-        }
-
-        for (int countMap : map.values()) {
-            if (countMap != 0) {
-                return false;
+        for (char symbolsOne : wordOne.toCharArray()) {
+            if (map.containsKey(symbolsOne)) {
+                map.put(symbolsOne, map.get(symbolsOne) + 1);
+            } else {
+                map.put(symbolsOne, 1);
             }
         }
-        return true;
+        for (char symbolsTwo : wordTwo.toCharArray()) {
+            if (!map.containsKey(symbolsTwo)) {
+                return false;
+            }
+            int symbolCount = map.get(symbolsTwo);
+            if (symbolCount > 1) {
+                map.put(symbolsTwo, symbolCount - 1);
+            } else {
+                map.remove(symbolsTwo);
+            }
+        }
+        return map.size() == 0;
     }
-
-
 }
+
