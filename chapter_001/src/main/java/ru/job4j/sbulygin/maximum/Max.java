263383e2a36f5e3cd244b3668863406792aebccc
sergeyBulygin <sergteletrade@gmail.com>
2018-12-01 13:32:10 +0300

    4.4. Add comments [# 190]

--- chapter_001/src/main/java/ru/job4j/sbulygin/maximum/Max.java
+++ chapter_001/src/main/java/ru/job4j/sbulygin/maximum/Max.java
@@ -8,10 +8,23 @@ package ru.job4j.sbulygin.maximum;
  */
 
 public class Max {
+    /**
+     * Maximum of two numbers.
+     * @param fist first numbers.
+     * @param second second numbers.
+     * @return maximum number.
+     */
     public int findMaximum(int fist, int second) {
         return fist > second ? fist : second;
     }
 
+    /**
+     * Maximum of three numbers.
+     * @param first first numbers.
+     * @param second second numbers.
+     * @param third third numbers.
+     * @return maximum number.
+     */
     public int findMaximum(int first, int second, int third) {
         int temp = findMaximum(first, second);
         temp = findMaximum(temp, third);
