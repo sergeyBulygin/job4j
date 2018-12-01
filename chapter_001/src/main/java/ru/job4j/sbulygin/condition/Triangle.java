58b2891afedfb0ca31d77a568791a86a58b651a0
sergeyBulygin <sergteletrade@gmail.com>
2018-12-01 13:08:14 +0300

    4.3. Add comments [# 9461]

--- chapter_001/src/main/java/ru/job4j/sbulygin/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/sbulygin/condition/Triangle.java
@@ -31,6 +31,10 @@ public class Triangle {
         return (ab + ac + bc) / 2;
     }
 
+    /**
+     * The method must calculate the area of the triangle.
+     * @return Return space if a triangle exists or -1 if there is no triangle.
+     */
     public double area() {
         double result = -1;
         double ab = this.a.distance(this.b);
@@ -43,6 +47,13 @@ public class Triangle {
         return result;
     }
 
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     * @param ab distance between points a b.
+     * @param ac distance between points a b.
+     * @param bc distance between points a b.
+     * @return boolean result.
+     */
     private boolean exist(double ab, double ac, double bc) {
         return (ab > 0 && ac > 0 && bc > 0 && ((ab + ac) != bc) && ((ac + bc) != ab) && ((ab + bc) != ac));
     }
