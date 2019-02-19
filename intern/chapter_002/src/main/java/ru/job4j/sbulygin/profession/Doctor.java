d15449f3c9c76ea4e1709d3cd53ee8713649262d
sergeyBulygin <sergteletrade@gmail.com>
2019-02-19 09:26:33 +0300

    Add new module chapter_002

---
+++ intern/chapter_002/src/main/java/ru/job4j/sbulygin/profession/Doctor.java
@@ -0,0 +1,15 @@
+package ru.job4j.sbulygin.profession;
+
+/**
+ * Class Doctor.
+ *
+ * @author anonymous.
+ * @version 1.0.
+ * @since 19.02.19.
+ */
+public class Doctor extends Profession {
+
+    public Doctor() {
+        this.name = "Doctor";
+    }
+}
---
+++ intern/chapter_002/src/main/java/ru/job4j/sbulygin/profession/Profession.java
@@ -0,0 +1,23 @@
+package ru.job4j.sbulygin.profession;
+
+/**
+ * Class Profession.
+ *
+ * @author anonymous.
+ * @version 1.0.
+ * @since 19.02.19.
+ */
+public class Profession {
+    /**
+     * Profession name.
+     */
+    private String name;
+
+    /**
+     * Getter for name.
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+}
